025a72d88d5e457bcfc9eed8d35d5486c71abb43
nathancarlos12 <nathan.lima@fcamara.com.br>
2019-05-11 18:47:57 -0300

    Add data to populate database

--- database/populate/category.js
+++ database/populate/category.js
@@ -1,6 +1,11 @@
 const categoryService = require('../../src/services/catalog/category')
 
 module.exports = async () => {
-  await categoryService.createCategory(1, 'Notebook')
+  await categoryService.createCategory(1, 'Notebook', 'http://3.17.48.179/media/catalog/category/notebook.png')
+  await categoryService.createCategory(1, 'Smartphones', 'http://3.17.48.179/media/catalog/category/telefonia.png')
+  await categoryService.createCategory(1, 'Games', 'http://3.17.48.179/media/catalog/category/games.png')
+  await categoryService.createCategory(1, 'Impressão e Imagem', 'http://3.17.48.179/media/catalog/category/impressao.png')
+  await categoryService.createCategory(1, 'Tv, Áudio e Vídeo', 'http://3.17.48.179/media/catalog/category/tvaudio.png')
+  await categoryService.createCategory(1, 'Informática', 'http://3.17.48.179/media/catalog/category/informatica.png')
   console.log('Category populated')
 }
--- src/models/catalog/product.js
+++ src/models/catalog/product.js
@@ -6,7 +6,7 @@ const Product = sqlize.define('products', {
   status: { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: true },
   sku: { type: Sequelize.STRING(50), allowNull: false },
   name: { type: Sequelize.STRING(255), allowNull: false },
-  content: { type: Sequelize.STRING, allowNull: false },
+  content: { type: Sequelize.STRING(2000), allowNull: false },
   price: { type: Sequelize.FLOAT, allowNull: false, validate: { isFloat: true } },
   quantity: { type: Sequelize.FLOAT, allowNull: false, validate: { isFloat: true }, defaultValue: 0 },
   image: { type: Sequelize.STRING },
