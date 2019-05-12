b44a5ae7fafa667bff4d66bfd96424a805d27060
nathancarlos12 <nathan.lima@fcamara.com.br>
2019-05-12 19:53:10 -0300

    Change type of number to string

--- src/models/user/address.js
+++ src/models/user/address.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize')
 const Address = sqlize.define('addresses', {
   postcode: { type: Sequelize.STRING(9), allowNull: false },
   street: { type: Sequelize.STRING(100), allowNull: false },
-  number: { type: Sequelize.INTEGER, allowNull: false, validate: { isInt: true } },
+  number: { type: Sequelize.STRING(20), allowNull: false },
   neighborhood: { type: Sequelize.STRING(100), allowNull: false },
   complement: { type: Sequelize.STRING(100), allowNull: true },
   city: { type: Sequelize.STRING(100), allowNull: false },
